f678d5f5530f6b0b15a0ab4e4c94e56db0596514
UltronTheDeath3000 <arina@pi.local>
2017-06-22 15:32:25 +0300

    Lesson1 20.06.17

---
+++ src/JavaCourse/Classwork.java
@@ -0,0 +1,10 @@
+package JavaCourse;
+
+/**
+ * Created by arina on 20.06.17.
+ */
+public class Classwork {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
